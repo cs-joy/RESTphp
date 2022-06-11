4836c500ef39f83ea62f5c3e2a989a73e191bebe
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-11 08:17:06 +0600

    add composer

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,16 @@
 <?php
-  
-  $db_user = 'admin';
-  $db_password = 'admin';
-  $db_name = 'rest_php';
 
-  $db = new PDO('mysql:host=localohost;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+  
+  $db_server = $_ENV['HOST'];
+  $db_user = $_ENV['USER'];
+  $db_password = $_ENV['PASSWORD'];
+  $db_name = $_ENV['DB'];
+
+  $db = new PDO('mysql:host='.$db_server.';dbname='.$db_name.';charset=utf8',$db_user,$db_password);
   
   //set some db attributes
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
